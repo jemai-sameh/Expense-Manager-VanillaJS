8b9e4fbe1c945d586ab77d54f7072040eefc012a
jemai-sameh <samehjemai98@gmail.com>
2023-09-11 22:14:36 +0100

    add alert message

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ An expense is defined by a title and a sum (a real one).
 A new expense is represented inthe interface by a Card, as shown in the figures, with two buttons Delete and Add.
 Each added expense is recorded in a list of expenses and the total sumsinserted is calculated as expenses are added.
 This application must be developed with Vanilla Javascript (without any framework).
+
+
+![Image 1](https://github.com/jemai-sameh/Expense-Manager-VanillaJS/image1.png) ![Image 2](https://github.com/jemai-sameh/Expense-Manager-VanillaJS/image2.png)
+
+
+![Image 3](https://github.com/jemai-sameh/Expense-Manager-VanillaJS/image3.png) ![Image 4](https://github.com/jemai-sameh/Expense-Manager-VanillaJS/image4.png)
--- app.js
+++ app.js
@@ -1,51 +1,46 @@
-const btnAdd=document.getElementById("btn-add");
-const btnCancel=document.getElementById("btn-cancel");
-const input_Title=document.getElementById("title-expense")
-const input_amount=document.getElementById("amount")
-const list=document.getElementById("list-expense")
-const total=document.getElementById("total")
-const CancelAllInput=()=>{
-    AlertMessage("Are you sure?","",[
-        {text:"Cancel"},{
-        text: 'OK',
-        handler: () => {
-            input_Title.value="";
-            input_amount.value="";
-                }}])
-    
+const btnAdd = document.getElementById("btn-add");
+const btnCancel = document.getElementById("btn-cancel");
+const input_Title = document.getElementById("title-expense")
+const input_amount = document.getElementById("amount")
+const list = document.getElementById("list-expense")
+const total = document.getElementById("total")
+let totalAmount = 0;
+
+const CancelAllInput = () => {
+    input_Title.value = "";
+    input_amount.value = "";
 }
-const  AlertMessage=async(header,message,buttons)=> {
+const AlertMessage = async (header, message, buttons) => {
     const alert = document.createElement('ion-alert');
     alert.header = header;
     alert.message = message;
     alert.buttons = buttons;
     document.body.appendChild(alert);
     await alert.present();
-  }
-let totalAmount=0;
+}
 
-btnCancel.addEventListener('click',CancelAllInput)
-btnAdd.addEventListener('click',()=>{
-    const title=input_Title.value;
-    const amount=input_amount.value;
+btnCancel.addEventListener('click',()=> AlertMessage("Are you sure?", "", [{ text: "Cancel" }, { text: 'OK', handler: () => CancelAllInput() }]))
+btnAdd.addEventListener('click', () => {
+    const title = input_Title.value;
+    const amount = input_amount.value;
 
-    if(!title.trim().length|| !amount.trim().length || amount==0){
-        AlertMessage("ERROR",'These fields are required!',['OK'])
+    if (!title.trim().length || !amount.trim().length) {
+        AlertMessage("ERROR", 'These fields are required!', ['OK'])
         return;
     }
-    if(amount==0){
-        AlertMessage("ERROR",'Amount must be greater than zero!',['OK'])
+    if (amount == 0) {
+        AlertMessage("ERROR", 'Amount must be greater than zero!', ['OK'])
         return;
     }
-    
+
     let newLabel = document.createElement("ion-label");
-    newLabel.textContent = `${title} : ${amount} $`; 
+    newLabel.textContent = `${title} : ${amount} $`;
     let newItem = document.createElement("ion-item");
     newItem.appendChild(newLabel);
     list.appendChild(newItem);
-    totalAmount+=+amount;
-    total.textContent=totalAmount;
-    total.style.color="red"
+    totalAmount += +amount;
+    total.textContent = totalAmount;
+    total.style.color = "red"
     CancelAllInput()
 
 })
